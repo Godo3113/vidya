61e7471de0b631f94b2007658ca7dac2a7c6297e
Deidad292 <37342309+Deidad292@users.noreply.github.com>
2020-03-09 19:35:23 +0100

    Attack animations, hitboxes, breakable pots

--- VidyaProject/Assets/Scripts/Player/PlayerMovement.cs
+++ VidyaProject/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,9 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState
+{
+	walk,
+	attack,
+	interact
+}
+
 public class PlayerMovement : MonoBehaviour
 {
-
+	private PlayerState currentState;
 	public float speed;
 	private Rigidbody2D myRigidbody;
 	private Vector3 change;
@@ -13,8 +20,11 @@ public class PlayerMovement : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
     {
+		currentState = PlayerState.walk;
 		animator = GetComponent<Animator>();
 		myRigidbody = GetComponent<Rigidbody2D>();
+		animator.SetFloat("moveX", 0);
+		animator.SetFloat("moveY", -1);
     }
 
     // Update is called once per frame
@@ -23,10 +33,26 @@ public class PlayerMovement : MonoBehaviour
 		change = Vector3.zero;
 		change.x = Input.GetAxisRaw("Horizontal");
 		change.y = Input.GetAxisRaw("Vertical");
-		UpdateAnimationAndMove();
+		if(Input.GetButtonDown("attack") && currentState != PlayerState.attack)
+        {
+			StartCoroutine(AttackCo());
+        }
+		else if (currentState == PlayerState.walk)
+        {
+			UpdateAnimationAndMove();
+		}
 
-		//Debug.Log(change);
 	}
+
+	private IEnumerator AttackCo()
+    {
+		animator.SetBool("attacking", true);
+		currentState = PlayerState.attack;
+		yield return null;
+		animator.SetBool("attacking", false);
+		yield return new WaitForSeconds(.3f);
+		currentState = PlayerState.walk;
+    }
 
 	void UpdateAnimationAndMove()
     {
@@ -46,6 +72,7 @@ public class PlayerMovement : MonoBehaviour
 
 	void MoveCharacter()
     {
+		change.Normalize();
 		myRigidbody.MovePosition(
 			transform.position + change * speed * Time.deltaTime);
     }

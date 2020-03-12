12bc0ad5bc114db263807842e85ed79049bc16f8
Deidad292 <37342309+Deidad292@users.noreply.github.com>
2020-03-12 02:35:16 +0100

    ContextTrigger and ContextClue
    - Added ContextTrigger to activate icons on objects/the palyer when near them.
    - Added text and working signs.
    - Added text title  when entering a room.

--- VidyaProject/Assets/Scripts/Camera/RoomMove.cs
+++ VidyaProject/Assets/Scripts/Camera/RoomMove.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RoomMove : MonoBehaviour
 {
     public Vector2 cameraChange;
     public Vector3 playerChange;
     private CameraMovement cam;
+    public bool needText;
+    public string placeName;
+    public GameObject text;
+    public Text placeText;
 
     // Start is called before the first frame update
     void Start()
@@ -22,11 +27,23 @@ public class RoomMove : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Player"))
+        if(other.CompareTag("Player") && !other.isTrigger)
         {
             cam.minPosition += cameraChange;
             cam.maxPosition += cameraChange;
             other.transform.position += playerChange;
+            if (needText)
+            {
+                StartCoroutine(placeNameCo());
+            }
         }
+    }
+
+    private IEnumerator placeNameCo()
+    {
+        text.SetActive(true);
+        placeText.text = placeName;
+        yield return new WaitForSeconds(3f);
+        text.SetActive(false);
     }
 }
